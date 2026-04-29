25e98b19290d051aaadf28b42043ce7975eb1f74
Azmain2005 <azmain.2005.mahtab@gmail.com>
2026-04-29 13:26:13 +0600

    updated admin navbar

--- app/components/AdminNavbar.js
+++ app/components/AdminNavbar.js
@@ -50,17 +50,17 @@ export default function AdminNavbar() {
 
 
             {/* Message (1) (Desktop) */}
-            <a href="#" className="flex items-center text-gray-600 hover:text-gray-800 text-sm">
+            {/* <a href="#" className="flex items-center text-gray-600 hover:text-gray-800 text-sm">
               <MessageSquare className="w-5 h-5 mr-1" />
               <span>Message (1)</span>
-            </a>
+            </a> */}
 
 
             {/* Profile (Desktop) */}
-            <a href="#" className="flex items-center text-gray-600 hover:text-gray-800 text-sm">
+            {/* <a href="#" className="flex items-center text-gray-600 hover:text-gray-800 text-sm">
               <User className="w-5 h-5 mr-1" />
               <span>Profile</span>
-            </a>
+            </a> */}
 
             {/* Logout (Desktop) */}
             <a
